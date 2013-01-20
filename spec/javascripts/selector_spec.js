2a7916e63ce1fd62dc163ece6f3fc58aaac8a8e1
calamari <g.tavonius@gmail.com>
2013-01-20 22:21:51 +0100

    Basic Selector behavior

--- spec/javascripts/selector_spec.js
+++ spec/javascripts/selector_spec.js
@@ -30,4 +30,116 @@ describe('Selector', function() {
       }
     });
   });
+
+  describe('when having a Selector with two nodes', function() {
+    var node, mayRun1, hasRun1, beSuccess1, startCalled1, endCalled1,
+        mayRun2, hasRun2, startCalled2, endCalled2;
+    beforeEach(function() {
+      mayRun1 = mayRun2 = beSuccess1 = true;
+      hasRun1 = startCalled1 = endCalled1 = false;
+      hasRun2 = startCalled2 = endCalled2 = false;
+      node = new BehaviorTree.Node({
+        canRun: function() { return mayRun1; },
+        run: function() {
+          hasRun1 = true;
+          if (beSuccess1) {
+            this.success();
+          } else {
+            this.fail();
+          }
+        },
+        start: function() { startCalled1 = true; },
+        end: function() { endCalled1 = true; }
+      });
+      selector = new BehaviorTree.Selector({
+        title: 'a selector',
+        nodes: [
+          node,
+          new BehaviorTree.Node({
+            canRun: function() { return mayRun2; },
+            run: function() { hasRun2 = true; this.success(); },
+            start: function() { startCalled2 = true; },
+            end: function() { endCalled2 = true; }
+          })
+        ]
+      });
+      selector.setControl({
+        success: function() {},
+        fail: function() {}
+      });
+    });
+
+    describe('if success is called by first task', function() {
+      beforeEach(function() {
+        beSuccess1 = true;
+        selector.run();
+      });
+
+      it("does not call start of the next task in line", function() {
+        expect(startCalled2).toBeFalsy();
+      });
+
+      it("does not call run of the next task in line", function() {
+        expect(hasRun2).toBeFalsy();
+      });
+    });
+
+    describe('if fail is called by first task', function() {
+      beforeEach(function() {
+        beSuccess1 = false;
+        selector.run();
+      });
+
+      it("calls start of the next task in line", function() {
+        expect(startCalled2).toBeTruthy();
+      });
+
+      it("calls run of the next task in line", function() {
+        expect(hasRun2).toBeTruthy();
+      });
+    });
+  });
+
+  describe('when in Selector with two nodes', function() {
+    var node, parentSelector, beSuccess, parentSuccessCalled, parentFailCalled;
+    beforeEach(function() {
+      beSuccess = true;
+      parentSuccessCalled = parentFailCalled = false;
+      selector = new BehaviorTree.Selector({
+        title: 'a selector',
+        nodes: [
+          new BehaviorTree.Node({
+            run: function() { if (beSuccess) { this.success(); } else { this.fail(); } }
+          })
+        ]
+      });
+
+      parentSelector = new BehaviorTree.Selector({
+        title: 'parent',
+        nodes: [ selector ],
+        success: function() {
+          parentSuccessCalled = true;
+        },
+        fail: function() {
+          parentFailCalled = true;
+        }
+      });
+    });
+
+    describe('one task results in success', function() {
+      it('calls success also in parent node', function() {
+        beSuccess = true;
+        parentSelector.run();
+        expect(parentSuccessCalled).toBeTruthy();
+      });
+    });
+
+    describe('all task result in failure', function() {
+      it('calls fail also in parent node', function() {
+        beSuccess = false;
+        parentSelector.run();
+        expect(parentFailCalled).toBeTruthy();
+      });
+    });
+  });
 });
