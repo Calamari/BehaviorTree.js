fbad504a0e986e63fea5ed76cb08f95a9e5f2e1b
Calamari <georg@tavonius.de>
2021-05-01 23:29:41 +0200

    Adapt Random node to new introspector

--- src/Random.js
+++ src/Random.js
@@ -4,21 +4,29 @@ import BranchNode from './BranchNode'
 export default class Random extends BranchNode {
   nodeType = 'Random'
 
-  run(blackboard = null, { indexes = [], rerun, registryLookUp = (x) => x } = {}) {
-    this.blueprint.start(blackboard)
+  run(blackboard = null, { indexes = [], introspector, rerun, registryLookUp = (x) => x } = {}) {
+    if (!rerun) this.blueprint.start(blackboard)
     let currentIndex = indexes.shift() || 0
     if (!rerun) {
       currentIndex = Math.floor(Math.random() * this.numNodes)
     }
     const node = registryLookUp(this.blueprint.nodes[currentIndex])
-    const result = node.run(blackboard, { indexes, rerun, registryLookUp })
+    const result = node.run(blackboard, { indexes, introspector, rerun, registryLookUp })
+    let overallResult = result
     if (result === RUNNING) {
-      return [currentIndex, ...indexes]
+      overallResult = [currentIndex, ...indexes]
     } else if (typeof result === 'object') {
       // array
-      return [...indexes, currentIndex, ...result]
+      overallResult = [...indexes, currentIndex, ...result]
     }
-    this.blueprint.end(blackboard)
-    return result
+    const isRunning = overallResult === RUNNING || typeof overallResult === 'object'
+    if (!isRunning) {
+      this.blueprint.end(blackboard)
+    }
+    if (introspector) {
+      const debugResult = isRunning ? RUNNING : overallResult
+      introspector.wrapLast(1, this, debugResult, blackboard)
+    }
+    return overallResult
   }
 }
